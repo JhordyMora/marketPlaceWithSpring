2ff170e8c9ce4082ceb88a1aebd939e4b201a699
JhordyMora <jhordy.mora@web.de>
2022-08-01 17:43:01 +0200

    First commit with basic configuarion of spring and creation of a test controller HolaMundoController

---
+++ mi-market/src/main/java/com/marketexample/mimarket/MiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.marketexample.mimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiMarketApplication.class, args);
+	}
+
+}
