064f1501e9e9dd3d00a3a2f13d09213d4a1046b1
JhordyMora <jhordy.mora@web.de>
2022-08-03 10:30:42 +0200

    added 2 last methods for ProductoRepository

--- src/main/java/com/marketexample/mimarket/persistence/ProductoRepository.java
+++ src/main/java/com/marketexample/mimarket/persistence/ProductoRepository.java
@@ -2,13 +2,41 @@ package com.marketexample.mimarket.persistence;
 
 import com.marketexample.mimarket.persistence.crud.ProductoCrudRepository;
 import com.marketexample.mimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
+// @Repository: le indicamos a la clase que es la encarga de interactuar con la base de datos.
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos (int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        // este metodo no se creo pq como la interfaz productoCrudRepository extiende de CrudRepository y esta
+        // ya tiene estos metodos
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        // este metodo no se creo pq como la interfaz productoCrudRepository extiende de CrudRepository y esta
+        // ya tiene estos metodos
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
--- src/main/java/com/marketexample/mimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/marketexample/mimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,7 +3,20 @@ package com.marketexample.mimarket.persistence.crud;
 import com.marketexample.mimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 // La interfaz crud repository pide que se le de la clase de la entity que se va a modificar, y el tipo de dato
-// de la variable de la primary key. Este interfaz lo que hace es importar medotos de crud para la tabla seleccionada
+// de la variable de la primary key. Este interfaz lo que hace es importar y definir metodos de crud para la tabla seleccionada
+// ademas de definir otros metodos mas que se desean tener para aplicarlos en las tablas (clases) del producto.
+// En este caso la clase ProductoRepository
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    // aqui estamos usando QueryMethods que nos ayudan a llamar informacion sin ayuda de SQL. Cuando estos se usan se
+    // debe respetar siempre el camelcase. Por lo tanto asi haya una variable que empieze con minuscula, se le debe
+    // poner mayuscula si es el caso. En el parentesis no se respeta el camelcase si no como se llama en la clase
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    //Opcional, se puede usar, en este caso el tag @Query(value="SELECT * FROM productos WHERE id_categoria = ?", nativeQuery=True)
+    // y luego crear la lista y poner cualquier nombre de metodo, aunque ahi no estariamos usando QueryMethods sino tags
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
